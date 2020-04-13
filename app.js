0b356498d2f240d2a1166a9ca92e26b8c46bc1f5
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-13 11:15:38 +0530

    usage of chalk

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const validator = require('validator')
-const getNotes = require('./notes.js')
+const chalk = require("chalk");
+const getNotes = require("./notes.js");
 
-const msg = getNotes()
-console.log(msg)
-
-console.log(validator.isURL('https/mead.io'))
+const msg = getNotes();
+console.log(chalk.yellow(msg));
+console.log(chalk.green("Success!"));
