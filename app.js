add48f975c939eee17930841940609ce52032b60
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-15 17:17:04 +0530

    handling user input

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const msg = getNotes();
-console.log(chalk.blue(msg));
-console.log(chalk.green("Success!"));
+//Change Yargs Version
+yargs.version("1.0.1");
 
-console.log(process.argv[2]);
+//ADD Command
+yargs.command({
+  command: "add",
+  description: "Add a new note",
+  builder: {
+    title: {
+      describe: "Title for the Note.",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Content for the Note.",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title, " Body: " + argv.body);
+  },
+});
 
-const command = process.argv[2];
+//Remove Command
+yargs.command({
+  command: "remove",
+  description: "Remove a note",
+  handler: function () {
+    console.log("removing a note!");
+  },
+});
 
-if (command === "add") {
-  console.log("Adding Note!");
-} else if (command === "remove") {
-  console.log("Remove Note!");
-}
+//Read Command
+yargs.command({
+  command: "read",
+  description: "Read Note",
+  handler: function () {
+    console.log("Read a Note!");
+  },
+});
+
+//List Command
+yargs.command({
+  command: "list",
+  description: "List Notes",
+  handler: function () {
+    console.log("Listing Notes!");
+  },
+});
+
+yargs.parse();
