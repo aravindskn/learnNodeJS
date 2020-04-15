a3591481d66371fcf772787a760dc0f70d5a1a44
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-15 11:55:34 +0530

    input from user

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const getNotes = require("./notes.js");
 const msg = getNotes();
 console.log(chalk.blue(msg));
 console.log(chalk.green("Success!"));
+
+console.log(process.argv[2]);
+
+const command = process.argv[2];
+
+if (command === "add") {
+  console.log("Adding Note!");
+} else if (command === "remove") {
+  console.log("Remove Note!");
+}
