6d316d2db10b1b8773100b895962a4b9ced528b2
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-17 15:09:31 +0530

    using http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,19 @@
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() => {
-  console.log("2 second timer");
-}, 2000);
+const url =
+  "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}358a4e311240293fdf5a6d4f5b8ec7e5";
 
-setTimeout(() => {
-  console.log("0 sec timer");
-}, 0);
+const url2 =
+  "https://api.weatherbit.io/v2.0/current?city_id=4487042&key=17f82e467ebe4560a98b755f448e4da6";
 
-console.log("Stopping");
+request({ url: url2, json: true }, (error, response) => {
+  const APIResponse = response.body.data[0];
+  console.log(
+    "It is currently " +
+      APIResponse.temp +
+      " degrees out. There is " +
+      APIResponse.precip +
+      "% chance of rain."
+  );
+  //   console.log(error);
+});
