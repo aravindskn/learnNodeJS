61d088d0ded76d75523a554a96e629705c00aa88
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-18 10:47:05 +0530

    added views and removed static pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,19 +5,30 @@ const app = express();
 
 const publicDirPath = path.join(__dirname, "../public");
 
+app.set("views", path.join(__dirname, "../views/"));
+app.set("view engine", "hbs");
 app.use(express.static(publicDirPath));
 
-// app.get("", (req, res) => {
-//   res.send("<h1>Weather</h1>");
-// });
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Aravind",
+  });
+});
 
-// app.get("/help", (req, res) => {
-//   res.send([{ name: "Aravind" }, { name: "Mahidhar" }]);
-// });
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    message: "This Page Provides Help",
+  });
+});
 
-// app.get("/about", (req, res) => {
-//   res.send("<h1>About</h1>");
-// });
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Page",
+    name: "Aravind",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send([{ forecast: "placeholder", location: "placeholder" }]);
