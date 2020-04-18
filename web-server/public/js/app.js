705c37210dbf59d215c7e278c8514abf1f6dbad4
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-18 13:13:34 +0530

    search weather from UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,29 @@
-console.log("Client Side JS File");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message");
+const messageTwo = document.querySelector("#loading");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  messageTwo.textContent = "Loading....";
+  messageOne.textContent = "";
+  fetch("http://localhost:8080/weather?address=" + location)
+    .then((res) => {
+      res.json().then((data) => {
+        if (data.error) {
+          messageTwo.textContent = "";
+          messageOne.textContent = data.error;
+        } else if (data.GeoCodeError) {
+          messageTwo.textContent = "";
+          messageOne.textContent = data.GeoCodeError;
+        } else {
+          messageTwo.textContent = "Location: " + data[0].location;
+          messageOne.textContent = "Forecast is: " + data[0].forecast;
+        }
+      });
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
+});
