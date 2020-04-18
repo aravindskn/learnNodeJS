c3053c65ea36ab9aaec0124b2b7de5cbd568fc08
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-18 22:58:31 +0530

    inserting docs into db

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,9 +14,58 @@ MongoClient.connect(
     }
     console.log("Connected to DB.");
     const db = client.db(database);
-    db.collection("users").insertOne({
-      name: "Aravind",
-      age: 23,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Aravind",
+    //     age: 23,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert User.");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Mahidhar",
+    //       age: 24,
+    //     },
+    //     {
+    //       name: "Ruthvik",
+    //       age: 23,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert docs.");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Task 1",
+          completed: false,
+        },
+        {
+          description: "Task 2",
+          completed: false,
+        },
+        {
+          description: "Task 3",
+          completed: false,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unabel to insert docs.");
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
