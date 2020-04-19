11bff8647e07f61cfdd72fe076449839c6236c4b
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-19 11:42:00 +0530

    db model valdiation

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const database = "task-manager-api";
 const connectionURL = "mongodb://127.0.0.1:27017/" + database;
@@ -8,21 +9,69 @@ mongoose.connect(connectionURL, {
   useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) throw new Error("Email was not valid.");
+//     },
+//     trim: true,
+//     lowercase: true,
+//   },
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) throw new Error("Age must be greater than 0.");
+//     },
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     minlength: [7, "Password must have greater than of 6 characters"],
+//     validate(value) {
+//       if (validator.contains(value, "password"))
+//         throw new Error("Password must not contain string password.");
+//     },
+//   },
+// });
+
+// const newUser = new User({
+//   name: "Ruthvik  ",
+//   email: "ruthvik@example.COM",
+//   password: "passwo",
+// });
+
+// newUser
+//   .save()
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+const Tasks = mongoose.model("Tasks", {
+  description: {
     type: String,
+    required: true,
+    trim: true,
   },
-  age: {
-    type: Number,
+  completed: {
+    type: Boolean,
+    default: false,
   },
 });
 
-const newUser = new User({
-  name: "Aravind",
-  age: "Test",
+const newTask = new Tasks({
+  description: "Task 2",
+  completed: true,
 });
 
-newUser
+newTask
   .save()
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
