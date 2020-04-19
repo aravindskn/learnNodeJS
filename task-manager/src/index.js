90a54d87f730477225305051de9f5e15e65600e5
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-19 12:42:23 +0530

    added apis for list

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,23 +9,61 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
 //User APIs
+
+//Create Users
 app.post("/users", (req, res) => {
   const user = new User(req.body);
   user
     .save()
-    .then(() => res.send(user))
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+    .then(() => res.status(201).send(user))
+    .catch((err) => res.status(400).send(err));
+});
+
+//List all Users
+app.get("/users", (req, res) =>
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => res.status(500).send(err))
+);
+
+//Fetch Particular User by ID
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) return res.status(404).send();
+      res.send(user);
+    })
+    .catch((err) => res.status(500).send());
 });
 
 //Tasks APIs
+
+//Create Tasks
 app.post("/tasks", (req, res) => {
   const task = new Tasks(req.body);
   task
     .save()
-    .then(() => res.send(task))
+    .then(() => res.status(201).send(task))
     .catch((err) => res.status(400).send(err));
+});
+
+//List all Tasks
+app.get("/tasks", (req, res) =>
+  Tasks.find({})
+    .then((tasks) => res.send(tasks))
+    .catch((err) => res.status(500).send())
+);
+
+//Fetch particular Task by ID
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Tasks.findById(_id)
+    .then((task) => {
+      if (!task) return res.status(404).send();
+      res.send(task);
+    })
+    .catch((err) => res.status(500).send());
 });
 
 app.listen(PORT, () => console.log("Server on PORT", PORT));
