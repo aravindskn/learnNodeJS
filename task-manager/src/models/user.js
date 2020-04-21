c33672fc6a918103ca9c1a9b781d49a564135d0c
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-21 09:41:42 +0530

    added timestamp

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -4,48 +4,51 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Tasks = require("./task");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate(value) {
-      if (!validator.isEmail(value)) throw new Error("Email was not valid.");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    trim: true,
-    lowercase: true,
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) throw new Error("Age must be greater than 0.");
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) throw new Error("Email was not valid.");
+      },
+      trim: true,
+      lowercase: true,
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: [7, "Password must have greater than of 6 characters"],
-    validate(value) {
-      if (validator.contains(value, "password"))
-        throw new Error("Password must not contain string password.");
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) throw new Error("Age must be greater than 0.");
       },
     },
-  ],
-});
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [7, "Password must have greater than of 6 characters"],
+      validate(value) {
+        if (validator.contains(value, "password"))
+          throw new Error("Password must not contain string password.");
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 userSchema.virtual("tasks", {
   ref: "Tasks",
