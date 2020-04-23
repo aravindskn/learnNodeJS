803b0f85afb241103516edca57a477c7ab06bd72
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-04-23 23:15:33 +0530

    first task test

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -5,4 +5,16 @@ const { userOneId, userOne, setUpDatabase } = require("./fixtures/db");
 
 beforeEach(setUpDatabase);
 
-test("Create Task for User", async () => {});
+test("Create Task for User", async () => {
+  const response = await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      description: "From Jest Test",
+    })
+    .expect(201);
+
+  const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+  expect(task.completed).toEqual(false);
+});
